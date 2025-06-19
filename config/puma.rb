ae2ddb2de84d1b27f83999da01aea96568c01f1b
shamindramatino <161926111+shamindramatino@users.noreply.github.com>
2025-06-19 22:38:41 +0530

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -18,6 +18,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
+bind "tcp://0.0.0.0:3000"
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
 
