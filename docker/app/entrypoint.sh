060d0a93b5c432aaf545e36d86aa32e604aaf9f7
shamindramatino <161926111+shamindramatino@users.noreply.github.com>
2025-06-19 22:47:30 +0530

    Update entrypoint.sh

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 set -e
 
-echo "bundle installation"
+echo "Bundle install"
 bundle check || bundle install
 
-echo "database migartion"
-bundle exec rails db:create
-bundle exec rails db:schema:load
+echo "Running migrations"
 bundle exec rails db:migrate
 
 if [ -f tmp/pids/server.pid ]; then
