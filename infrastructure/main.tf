f281709add5da15089cca2a6e9fbb32c4d0bade4
shamindramatino <161926111+shamindramatino@users.noreply.github.com>
2025-06-19 22:52:47 +0530

    Update main.tf

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -211,8 +211,8 @@ resource "aws_ecs_task_definition" "app" {
   family                   = "ror-task"
   requires_compatibilities = ["FARGATE"]
   network_mode             = "awsvpc"
-  cpu                      = "512"
-  memory                   = "1024"
+  cpu                      = "1024"
+  memory                   = "2048"
   execution_role_arn       = aws_iam_role.ecs_exec.arn
 
   container_definitions = jsonencode([
